b7524531b9ded8d84db213cabd7fa0e0b9dd3f76
Armano den Boef <68127614+Rozen4334@users.noreply.github.com>
2023-03-03 21:50:38 +0100

    2/2

--- src/NetCord.Addons.Hosting/TokenConfigurationOptions.cs
+++ src/NetCord.Addons.Hosting/TokenConfigurationOptions.cs
@@ -1,12 +1,12 @@
 ﻿namespace NetCord.Addons.Hosting
 {
     /// <summary>
-    ///     Represents configuration bound options that assist in resolving 
+    ///     Represents configuration bound options that assist in resolving the token in the <see cref="TokenFactory"/>.
     /// </summary>
     public class TokenConfigurationOptions
     {
         /// <summary>
-        ///     
+        ///     The raw token received through the <see cref="GatewayHostingContext"/>.
         /// </summary>
         public string Token { get; set; } = string.Empty;
     }
